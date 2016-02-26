907edae92350beb93657af98ea36e58e0e783e26
Laurensesss <veryverygood@qq.com>
2016-02-26 16:06:37 +0800

    Update 一个劳伦斯-week1-1.c

--- 一个劳伦斯-week1-1.c
+++ 一个劳伦斯-week1-1.c
@@ -1,13 +1,13 @@
 //Reverse_the_String
 #include<stdio.h>
 #include<string.h>
-
+#define LENGTH 1000
 void Reverse_the_String(char a_string[], char r_string[], unsigned long int a_length);
 
 int main(int argc, const char * argv[])
 {
-    char a_string[1000];
-    char r_string[1000];
+    char a_string[LENGTH];
+    char r_string[LENGTH];
     gets(a_string);
     unsigned long int a_length = strlen(a_string);
     Reverse_the_String(a_string,r_string,a_length);
@@ -29,7 +29,7 @@ int main(int argc, const char * argv[])
     return 0;
 }
 
-void Reverse_the_String(char a_string[1000], char r_string[1000], unsigned long int a_length)
+void Reverse_the_String(char a_string[LENGTH], char r_string[LENGTH], unsigned long int a_length)
 {
     for(int i = 0; i < a_length; i++)
     {
