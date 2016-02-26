e5bde9ea6505f27081bd04556b71c8ef6bc5ddd0
Laurensesss <veryverygood@qq.com>
2016-02-26 18:57:07 +0800

    Update 一个劳伦斯-week1-2.c

--- 一个劳伦斯-week1-2.c
+++ 一个劳伦斯-week1-2.c
@@ -5,18 +5,21 @@
 
 int main()
 {
-    char input[3*LENGTH+4];
-    char mess_pw[LENGTH];
-    int rank[LENGTH];
-    int times;
-    gets(input);
-    unsigned long int input_length = strlen(input);
-    unsigned long int pw_length = (input_length-4)/3;
+    char input[3*LENGTH+4];                                         //声明input字符串以接收所有输入
+    gets(input);                                                    //一次获得所有输入
+    
+    char mess_pw[LENGTH];                                           //声明mess_pw表示乱码字符串
+    int rank[LENGTH];                                               //声明rank表示数组
+    int times;                                                      //声明times表示整数
+    unsigned long int input_length = strlen(input);                 //声明input_length以确定输入的input字符串的长度
+    unsigned long int pw_length = (input_length-4)/3;               //确定mess_pw以及rank的长度
+    /*-----从input中解析出mess_pw-----*/
     for(int i=0; i<pw_length+2; i++)
     {
         if(input[i] != '"')
             mess_pw[i-1] = input[i];
     }
+    /*-----从input中解析出rank-----*/
     int count = 0;
     for(unsigned long int i=pw_length+2; i<input_length-1; i++)
     {
@@ -33,9 +36,11 @@ int main()
         else
             continue;
     }
+    /*-----从input中解析出times-----*/
     times = input[input_length-1]-48;
-    char pw[LENGTH];
     
+    char pw[LENGTH];                                                //声明pw以表示破解后的密码
+    /*-----进行mess_pw的破解，并赋值给pw-----*/
     while(times>0)
     {
         for(int i=0; i<pw_length; i++)
@@ -48,9 +53,11 @@ int main()
         }
         times--;
     }
+    /*-----输出破解后的密码-----*/
     printf("\"");
     for(int i=0; i<pw_length; i++)
         printf("%c", pw[i]);
     printf("\"\n");
+    /*-----END!!!-----*/
     return 0;
 }
