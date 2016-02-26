30a5af54777f4898c4fc0b3cfb337fb9854dee9d
Laurensesss <veryverygood@qq.com>
2016-02-26 19:08:41 +0800

    Update 一个劳伦斯-week1-1.c

--- 一个劳伦斯-week1-1.c
+++ 一个劳伦斯-week1-1.c
@@ -10,15 +10,16 @@ int main(int argc, const char * argv[])
     char r_string[LENGTH];
     gets(a_string);
     unsigned long int a_length = strlen(a_string);
+    //调用Reverse_the_String函数，将a_string的倒序赋值给r_string
     Reverse_the_String(a_string,r_string,a_length);
+    
     int count = 0;
     for(int i = 0; i < strlen(a_string); i++)
     {
         if(a_string[i] == r_string[i])
-        {
             count ++;
-        }
     }
+    //通过判断count的值与a_string长度的比较以确定是否为回文，并输出对应字符串
     if(count == strlen(a_string))
     {
         printf("palindrome");
