b39fbc3ef8a43199bc8c319c9e10db3254b1ad7c
CWB <1141644656@qq.com>
2016-03-01 17:45:56 +0800

    Update week0_1.c

--- week0_1.c
+++ week0_1.c
@@ -15,12 +15,12 @@ int function(int x, int array[], int length)
 
 int mycomp(const void * p1, const void * p2)
 {
-  const int * a1 = (const int *) p1;
-	const int * a2 = (const int *) p2;
-	if (*a1 < *a2)
-    return -1;
-  else if (*a1 == *a2)
-    return 0;
-  else
-    return 1;
+	const int * a1 = (const int *) p1;
+  	const int * a2 = (const int *) p2;
+  	if (*a1 < *a2)
+    		return -1;
+  	else if (*a1 == *a2)
+    		return 0;
+  	else
+    		return 1;
 }
