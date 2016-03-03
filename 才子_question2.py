8ee792e74f801c28fdfd27f075f354210d399f98
luhuiyang <ylhyajayaj@gmail.com>
2016-03-03 11:28:56 +0800

    Create 才子_question2.py
    迟来的作业2

---
+++ 才子_question2.py
@@ -0,0 +1,15 @@
+def last(x, step):
+    if x <= 0 or step <= 1:return 'params error'
+    
+    temp_step = step - 1
+    
+    for i in range(x):
+        nums.append(i+1)
+
+    while len(nums) > 1:
+        if temp_step - len(nums) >=0:
+            temp_step = temp_step - len(nums)
+        else:
+            nums.pop(temp_step)
+            temp_step += step -1
+    return nums[0]
