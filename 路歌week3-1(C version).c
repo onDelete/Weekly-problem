c47469245d6a044065b4bc15dde07ab43af3e256
Roadsong <260625918@qq.com>
2016-03-08 08:14:16 +0800

    Update 路歌week3-1(C version).c

--- 路歌week3-1(C version).c
+++ 路歌week3-1(C version).c
@@ -15,31 +15,40 @@ int main()
         int len_b = 0;
         
         int i = 0;
-        while((c=getchar())!=EOF && c != '\n')
-        {
+        while((c=getchar())!=EOF && c != '\n') {
                 a[i++] = c;
                 len_a++;
         }
 
+
         i = 0;
-        while((c=getchar())!=EOF && c != '\n')
-        {
+        while((c=getchar())!=EOF && c != '\n') {
                 b[i++] = c;
                 len_b++;
         }
+
         
+        /* 声明int型变量key，代表状态 */
         int key = 0;
-        for(i = 0; i < len_a - len_b; i++)
-        {
+        
+        for(i = 0; i < len_a - len_b; i++) {
+                
+                /* 用memcmp函数将a，b两个数组的相应位置的字符进行比较 */
                 int temp_result = memcmp(a + i,b,len_b);
-                if(temp_result == 0)
-                {
+                
+                /* 若相等，key赋值为1，退出循环 */
+                if(temp_result == 0) {
                         key = 1;
                         break;  
                 }
         }
         
+        
+        /* 输出key的值 */
         printf("%d",key);       
 
+
+        /* 程序结束 */
         return 0;
 }
+
