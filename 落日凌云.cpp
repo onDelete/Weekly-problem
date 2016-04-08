43bdd6c0ec567c07f2c5400729e2a3ba98f00b65
wtlusvm <wtlusvm@users.noreply.github.com>
2016-04-08 09:24:28 +0800

    Update 落日凌云.cpp

--- 落日凌云.cpp
+++ 落日凌云.cpp
@@ -13,43 +13,46 @@
 //* 这个问题可以直接 2 << 1 得到结果，这是最简单的情况了。
 //* 有些情况也要分情况讨论。
 //* 
-//* 我们只用考虑整数的运算，除法不用考虑有小数的情况，比如不用考虑5 / 3，但是如果你想挑战的话也可以做一下，5 / 3 = 1。
-//* 注意：不能用 + -*/ 这些符号，而且要考虑各种特殊情况，比如 2 * 3就不能简单的用 << 做。希望大家不要胡做，好好想一下。
+//* 我们只用考虑整数的运算，除法不用考虑有小数的情况，比如不用考虑5 / 3，
+//* 但是如果你想挑战的话也可以做一下，5 / 3 = 1。
+//* 注意：不能用 + -*/ 这些符号，而且要考虑各种特殊情况，比如 2 * 3就不能简单的用 << 做。
+//* 希望大家不要胡做，好好想一下。
+//* 
+//* 注意：本程序支持括号
 //* 
 
 #include <iostream>
 #include <string>
-#include <algorithm>
 #include "Week6.h"
 
 int main()
 {
 	//* Code for debug
-	//* int num1 = 6, num2 = 3;
-	//* int num3 = -6, num4 = -3;
-	//* std::cout << "Code for debug:" << std::endl;
-	//* std::cout << "----BEGIN----" << std::endl;
-	//* std::cout << Add(num1, num2) << std::endl;//* 9
-	//* std::cout << Add(num3, num4) << std::endl;//* -9
-	//* std::cout << Add(num1, num4) << std::endl;//* 3
-	//* std::cout << Add(num3, num2) << std::endl;//* -3
-	//* std::cout << Minus(num1, num2) << std::endl;//* 3
-	//* std::cout << Minus(num3, num4) << std::endl;//* -3
-	//* std::cout << Minus(num2, num3) << std::endl;//* 9
-	//* std::cout << Minus(num3, num2) << std::endl;//* -9
-	//* std::cout << Multiply(num1, num2) << std::endl;//* 18
-	//* std::cout << Multiply(num3, num4) << std::endl;//* 18
-	//* std::cout << Multiply(num1, num4) << std::endl;//* -18
-	//* std::cout << Multiply(num3, num2) << std::endl;//* -18
-	//* std::cout << Divide(num1, num2) << std::endl;//* 2
-	//* std::cout << Divide(num2, num1) << std::endl;//* 0
-	//* std::cout << Divide(num3, num4) << std::endl;//* 2
-	//* std::cout << Divide(num4, num3) << std::endl;//* 0
-	//* std::cout << Divide(num1, num4) << std::endl;//* -2
-	//* std::cout << Divide(num4, num1) << std::endl;//* 0
-	//* std::cout << Divide(num3, num2) << std::endl;//* -2
-	//* std::cout << Divide(num2, num3) << std::endl;//* 0
-	//* std::cout << "----END----" << std::endl;
+	//int num1 = 6, num2 = 3;
+	//int num3 = -6, num4 = -3;
+	//std::cout << "Code for debug:" << std::endl;
+	//std::cout << "----BEGIN----" << std::endl;
+	//std::cout << Add(num1, num2) << std::endl;//* 9
+	//std::cout << Add(num3, num4) << std::endl;//* -9
+	//std::cout << Add(num1, num4) << std::endl;//* 3
+	//std::cout << Add(num3, num2) << std::endl;//* -3
+	//std::cout << Minus(num1, num2) << std::endl;//* 3
+	//std::cout << Minus(num3, num4) << std::endl;//* -3
+	//std::cout << Minus(num2, num3) << std::endl;//* 9
+	//std::cout << Minus(num3, num2) << std::endl;//* -9
+	//std::cout << Multiply(num1, num2) << std::endl;//* 18
+	//std::cout << Multiply(num3, num4) << std::endl;//* 18
+	//std::cout << Multiply(num1, num4) << std::endl;//* -18
+	//std::cout << Multiply(num3, num2) << std::endl;//* -18
+	//std::cout << Divide(num1, num2) << std::endl;//* 2
+	//std::cout << Divide(num2, num1) << std::endl;//* 0
+	//std::cout << Divide(num3, num4) << std::endl;//* 2
+	//std::cout << Divide(num4, num3) << std::endl;//* 0
+	//std::cout << Divide(num1, num4) << std::endl;//* -2
+	//std::cout << Divide(num4, num1) << std::endl;//* 0
+	//std::cout << Divide(num3, num2) << std::endl;//* -2
+	//std::cout << Divide(num2, num3) << std::endl;//* 0
+	//std::cout << "----END----" << std::endl;
 
 	std::string input;
 	std::getline(std::cin, input);
